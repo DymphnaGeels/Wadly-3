b6e4309227d13b72ad288e0c94e0b5e34598d582
DymphnaGeels <110533@st-maartenscollege.nl>
2021-05-19 10:17:06 +0200

    Stap 1 t/m 5 helemaal, bezig met stap 6. Hiervoor is committen blijkbaar mislukt helaas.. sorry:(-

--- Wadly-3/Controllers/HomeController.cs
+++ Wadly-3/Controllers/HomeController.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using Festivalsite.Models;
+using MySql.Data;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Festivalsite.Database;
 using Wadly_3.Models;
 
-namespace Wadly_3.Controllers
+namespace Festivalsite.Controllers
 {
     public class HomeController : Controller
     {
@@ -20,10 +22,131 @@ namespace Wadly_3.Controllers
 
         public IActionResult Index()
         {
+            // alle namen ophalen
+            var products = GetProducts();
+
+            // stop de namen in de html
+            return View(products);
+        }
+
+
+        public IActionResult Privacy()
+        {
             return View();
         }
 
-        public IActionResult Privacy()
+        public List<Product> GetProducts()
+        {
+            // stel in waar de database gevonden kan worden
+            string connectionString = "Server=informatica.st-maartenscollege.nl;Port=3306;Database=110533;Uid=110533;Pwd=inf2021sql";
+
+            // maak een lege lijst waar we de namen in gaan opslaan
+            List<Product> products = new List<Product>();
+
+            // verbinding maken met de database
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                // verbinding openen
+                conn.Open();
+
+                // SQL query die we willen uitvoeren
+                MySqlCommand cmd = new MySqlCommand("select * from product", conn);
+
+                // resultaat van de query lezen
+                using (var reader = cmd.ExecuteReader())
+                {
+                    // elke keer een regel (of eigenlijk: database rij) lezen
+                    while (reader.Read())
+                    {
+                        Product p = new Product
+                        {
+                            // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
+                            Id = Convert.ToInt32(reader["Id"]),
+                            Beschikbaarheid = Convert.ToInt32(reader["Beschikbaarheid"]),
+                            Naam = reader["Naam"].ToString(),
+                            Prijs = reader["Prijs"].ToString(),
+                        };
+
+                        // voeg de naam toe aan de lijst met namen
+                        products.Add(p);
+                    }
+                }
+            }
+
+            // return de lijst met namen
+            return products;
+        }
+
+        public List<string> GetNames()
+        {
+            // stel in waar de database gevonden kan worden
+            string connectionString = "Server=172.16.160.21;Port=3306;Database=fastfood;Uid=lgg;Pwd=0P%Y9fI2GdO#;";
+
+            // maak een lege lijst waar we de namen in gaan opslaan
+            List<string> names = new List<string>();
+
+            // verbinding maken met de database
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                // verbinding openen
+                conn.Open();
+
+                // SQL query die we willen uitvoeren
+                MySqlCommand cmd = new MySqlCommand("select * from product", conn);
+
+                // resultaat van de query lezen
+                using (var reader = cmd.ExecuteReader())
+                {
+                    // elke keer een regel (of eigenlijk: database rij) lezen
+                    while (reader.Read())
+                    {
+                        // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
+                        string Name = reader["Naam"].ToString();
+
+                        // voeg de naam toe aan de lijst met namen
+                        names.Add(Name);
+                    }
+                }
+            }
+
+            // return de lijst met namen
+            return names;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
+[Route("films")]
+        public IActionResult Films()
+        {
+            return View();
+        }
+
+        [Route("genres")]
+        public IActionResult genres()
+        {
+            return View();
+        }
+
+        [Route("Events")]
+        public IActionResult Events()
+        {
+            return View();
+        }
+
+        [Route("Unlimited")]
+        public IActionResult Unlimited()
+        {
+            return View();
+        }
+
+        [Route("account")]
+        public IActionResult account()
         {
             return View();
         }
