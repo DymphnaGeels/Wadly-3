cecaed357a2ef8eef1b25c0a92491c2d5d5d725e
110533@st-maartenscollege.nl <110533@st-maartenscollege.nl>
2021-05-19 10:52:47 +0200

    Het oplossen van 20 errors in de homeController

--- Wadly-3/Controllers/HomeController.cs
+++ Wadly-3/Controllers/HomeController.cs
@@ -1,13 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
-using Festivalsite.Models;
+using Wadly_3.Models;
 using MySql.Data;
 using System;
 using System.Collections.Generic;
 using MySql.Data.MySqlClient;
-using Festivalsite.Database;
-using Wadly_3.Models;
+using Wadly_3.Database;
 
 namespace Festivalsite.Controllers
 {
@@ -23,7 +22,7 @@ namespace Festivalsite.Controllers
         public IActionResult Index()
         {
             // alle namen ophalen
-            var products = GetProducts();
+            var products = GetNames();
 
             // stop de namen in de html
             return View(products);
@@ -35,13 +34,13 @@ namespace Festivalsite.Controllers
             return View();
         }
 
-        public List<Product> GetProducts()
+        public List<Film> GetNames()
         {
             // stel in waar de database gevonden kan worden
             string connectionString = "Server=informatica.st-maartenscollege.nl;Port=3306;Database=110533;Uid=110533;Pwd=inf2021sql";
 
             // maak een lege lijst waar we de namen in gaan opslaan
-            List<Product> products = new List<Product>();
+            List<Film> Film = new List<Film>();
 
             // verbinding maken met de database
             using (MySqlConnection conn = new MySqlConnection(connectionString))
@@ -50,7 +49,7 @@ namespace Festivalsite.Controllers
                 conn.Open();
 
                 // SQL query die we willen uitvoeren
-                MySqlCommand cmd = new MySqlCommand("select * from product", conn);
+                MySqlCommand cmd = new MySqlCommand("select * from film", conn);
 
                 // resultaat van de query lezen
                 using (var reader = cmd.ExecuteReader())
@@ -58,7 +57,7 @@ namespace Festivalsite.Controllers
                     // elke keer een regel (of eigenlijk: database rij) lezen
                     while (reader.Read())
                     {
-                        Product p = new Product
+                        Film p = new Film
                         {
                             // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
                             Id = Convert.ToInt32(reader["Id"]),
@@ -77,7 +76,7 @@ namespace Festivalsite.Controllers
             return products;
         }
 
-        public List<string> GetNames()
+        public List<string> GetFilmsinfo()
         {
             // stel in waar de database gevonden kan worden
             string connectionString = "Server=172.16.160.21;Port=3306;Database=fastfood;Uid=lgg;Pwd=0P%Y9fI2GdO#;";
@@ -118,13 +117,13 @@ namespace Festivalsite.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-    }
-}
+ 
 
-[Route("films")]
+
+        [Route("films")]
         public IActionResult Films()
         {
-            return View();
+        return View();
         }
 
         [Route("genres")]
@@ -151,10 +150,5 @@ namespace Festivalsite.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
