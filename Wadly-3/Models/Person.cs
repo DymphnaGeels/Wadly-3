bbcaf867b879cec8513928e84d6fcc5900cae14f
110533@st-maartenscollege.nl <110533@st-maartenscollege.nl>
2021-05-28 13:06:19 +0200

    errormessage toegevoegd

--- Wadly-3/Models/Person.cs
+++ Wadly-3/Models/Person.cs
@@ -4,17 +4,15 @@ namespace SendAndStore.Models
 {
     public class Person
     {
-        [Required]
+        [Required(ErrorMessage = "Gelieve uw voornaam in te vullen")]
         public string FirstName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Gelieve uw achternaam in te vullen")]
         public string LastName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Gelieve uw Email in te vullen")]
         public string Email { get; set; }
-        [Required]
         public string Phone { get; set; }
-        [Required]
         public string Address { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Gelieve uw beschrijving in te vullen")]
         public string Description { get; set; }
     }
 }
